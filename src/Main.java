ae9eb712426fa185118a92545d88764fded18f33
ColdEsper <shadowguardian@live.com>
2015-11-06 04:14:50 -0800

    add JFrame to main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+
+class Graph extends JPanel {
+	public void paintComponent (Graphics g) {
+		super.paintComponent(g);
+		int width = getWidth();
+		int height = getHeight();
+		g.drawString("X-Axis",width/2,height);
+		g.drawString("Y-Axis",0,height/2);
+	}
+}
+
 public class Main {
 	Cycle cyc;
 	public static void main(String args[]) {
-		System.out.println("Main function called!");
+		JFrame frame = new JFrame("Cycle Calculation");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Graph graph = new Graph();
+		frame.add(graph);
+		frame.setSize(640,640);
+		frame.setVisible(true);
 	}
 }
+
