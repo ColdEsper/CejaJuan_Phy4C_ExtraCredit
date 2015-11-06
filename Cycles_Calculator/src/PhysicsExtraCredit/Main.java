3610a4b676924c5e5c9cbade5c4a79641f484328
ColdEsper <shadowguardian@live.com>
2015-11-06 14:53:19 -0800

    Grid added to graph

--- Cycles_Calculator/src/PhysicsExtraCredit/Main.java
+++ Cycles_Calculator/src/PhysicsExtraCredit/Main.java
@@ -7,16 +7,28 @@ import javax.swing.*;
 class Graph extends JPanel {
 	String XAxis;
 	String YAxis;
-	public Graph (String XAxisName, String YAxisName) {
+	int gridSpace;
+	public Graph (String XAxisName, String YAxisName, int gridSpacing) {
 		XAxis = XAxisName;
 		YAxis = YAxisName;
+		gridSpace = gridSpacing;
 	}
 	public void paintComponent (Graphics g) {
 		super.paintComponent(g);
+		FontMetrics metrics = g.getFontMetrics();
 		int width = getWidth();
 		int height = getHeight();
 		g.drawString(XAxis,width/2,height);
 		g.drawString(YAxis,0,height/2);
+		//draw grid of graph
+		for (int x=metrics.stringWidth(YAxis)+3;x<width-gridSpace;x+=gridSpace) {
+			for (int y=metrics.getHeight();y<height-metrics.getHeight()-gridSpace;y+=gridSpace) {
+				g.drawLine(x,y,x+gridSpace,y);
+				g.drawLine(x+gridSpace,y,x+gridSpace,y+gridSpace);
+				g.drawLine(x,y,x,y+gridSpace);
+				g.drawLine(x,y+gridSpace,x+gridSpace,y+gridSpace);
+			}
+		}
 	}
 }
 
@@ -26,7 +38,7 @@ public class Main {
 		if (args.length > 0 && args[0].toLowerCase().equals("gui")) {
 			JFrame frame = new JFrame("Cycle Calculation");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			Graph graph = new Graph("X-Axis","Y-Axis");
+			Graph graph = new Graph("X-Axis","Y-Axis",20);
 			frame.add(graph);
 			frame.setSize(640,640);
 			frame.setVisible(true);
