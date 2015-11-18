f3cd6a7e35e758969105c5d593d63948dcaf8db2
ColdEsper <shadowguardian@live.com>
2015-11-17 22:14:57 -0800

    verify todo added

--- Cycles_Calculator/src/PhysicsExtraCredit/CycleData.java
+++ Cycles_Calculator/src/PhysicsExtraCredit/CycleData.java
@@ -7,6 +7,11 @@ public class CycleData {
 		processData = new ArrayList<CycleProcess>();
 		nodeData = new ArrayList<CycleNode>();
 	}
+	public boolean verify () {
+		//TODO check that loaded data is sensible.
+		//For example, check the values don't violate any laws of physics
+		return true;
+	}
 	ArrayList<CycleProcess> processData;
 	ArrayList<CycleNode> nodeData;
 	float moles;
