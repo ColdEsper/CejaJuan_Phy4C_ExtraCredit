c83b96d1a82f6ad53ca88f00d60cdb0f94285a51
ColdEsper <shadowguardian@live.com>
2015-11-23 23:00:43 -0800

    refactoring of process panel

--- Cycles_Calculator/src/PhysicsExtraCredit/Main.java
+++ Cycles_Calculator/src/PhysicsExtraCredit/Main.java
@@ -5,23 +5,62 @@ import java.awt.*;
 import java.io.IOException;
 import javax.swing.*;
 
+class ProcessPanel extends Panel{
+	public ProcessPanel (CycleProcess process) {
+		setLayout(new GridBagLayout());
+		GridBagConstraints layout = new GridBagConstraints();
+		layout.weightx=1.0;
+		layout.weighty=1.0;
+		layout.gridwidth = 1;
+		layout.gridheight = 1;
+		layout.gridx = 0;
+		layout.gridy = 0;
+		layout.ipadx=2;
+		layout.ipady=20;
+		switch (process.type) {
+			case ADIABATIC:
+				add(new JLabel("Adiabatic"),layout);
+				break;
+			case ISOTHERMAL:
+				add(new JLabel("Isothermal"),layout);
+				break;
+			case ISOBARIC:
+				add(new JLabel("Isobaric"),layout);
+				break;
+			case ISOCHORIC:
+				add(new JLabel("Isochoric"),layout);
+				break;
+		}
+		layout.ipady=1;
+		layout.gridy+=1;
+		add(new JLabel("Start: "+process.start.name),layout);
+		layout.gridx+=1;
+		add(new JLabel("End: "+process.end.name),layout);
+		layout.gridx=0;
+		layout.gridy+=1;
+		add(new JLabel("Heat: "+process.heatChange),layout);
+		layout.gridx+=1;
+		add(new JLabel("Work: "+process.heatChange),layout);
+		layout.gridx+=1;
+		add(new JLabel("Energy: "+process.energyChange),layout);
+	}
+}
+
 public class Main {
 	Cycle cyc;
 	public static void main(String args[]) throws IOException {
 		if (args.length > 0 && args[0].toLowerCase().equals("gui")) {
 			JFrame frame = new JFrame("Cycle Calculation");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			GridBagLayout layout = new GridBagLayout();
-			frame.setLayout(layout);
-			GridBagConstraints layoutRules = new GridBagConstraints();
-			layoutRules.weightx=1.0;
-			layoutRules.weighty=1.0;
-			layoutRules.gridwidth = 1;
-			layoutRules.gridheight = 1;
-			layoutRules.gridx = 0;
-			layoutRules.gridy = 0;
-			layoutRules.ipadx=2;
-			layoutRules.ipady=1;
+			GridBagLayout gridBag = new GridBagLayout();
+			frame.setLayout(gridBag);
+			GridBagConstraints layout = new GridBagConstraints();
+			layout.weightx=1.0;
+			layout.weighty=1.0;
+			layout.gridwidth = 1;
+			layout.gridheight = 1;
+			layout.gridx = 0;
+			layout.gridy = 0;
 			JFileChooser chooser = new JFileChooser();
 			int option = chooser.showOpenDialog(frame);
 			CycleData data;
@@ -40,43 +79,16 @@ public class Main {
 				return;
 			}
 			Cycle cyc = new Cycle(data);
-			JPanel procPanel = new JPanel(layout);
+			JPanel procsPanel = new JPanel(gridBag);
 			for (int i=0;i<cyc.processes.size();++i) {
 				CycleProcess proc = cyc.processes.get(i);
-				layoutRules.ipady=20;
-				switch (proc.type) {
-					case ADIABATIC:
-						procPanel.add(new JLabel("Adiabatic"),layoutRules);
-						break;
-					case ISOTHERMAL:
-						procPanel.add(new JLabel("Isothermal"),layoutRules);
-						break;
-					case ISOBARIC:
-						procPanel.add(new JLabel("Isobaric"),layoutRules);
-						break;
-					case ISOCHORIC:
-						procPanel.add(new JLabel("Isochoric"),layoutRules);
-						break;
-				}
-				layoutRules.ipady=1;
-				layoutRules.gridy+=1;
-				procPanel.add(new JLabel("Start: "+proc.start.name),layoutRules);
-				layoutRules.gridx+=1;
-				procPanel.add(new JLabel("End: "+proc.end.name),layoutRules);
-				layoutRules.gridx=0;
-				layoutRules.gridy+=1;
-				procPanel.add(new JLabel("Heat: "+proc.heatChange),layoutRules);
-				layoutRules.gridx+=1;
-				procPanel.add(new JLabel("Work: "+proc.heatChange),layoutRules);
-				layoutRules.gridx+=1;
-				procPanel.add(new JLabel("Energy: "+proc.energyChange),layoutRules);
-				layoutRules.gridx=0;
-				layoutRules.gridy+=1;
+				procsPanel.add(new ProcessPanel(proc),layout);
+				layout.gridy+=1;
 			}
-			layoutRules.gridx=0;
-			layoutRules.gridy=0;
-			layoutRules.anchor=GridBagConstraints.NORTH;
-			frame.add(procPanel,layoutRules);
+			layout.gridx=0;
+			layout.gridy=0;
+			layout.anchor=GridBagConstraints.NORTH;
+			frame.add(procsPanel,layout);
 			frame.setSize(640,640);
 			frame.setVisible(true);
 		} else {
